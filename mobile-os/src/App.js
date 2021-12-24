28fced00c8c84f76365c81a3c3540e7c682b23a5
Swathi <swathi709357@gmail.com>
2021-12-24 14:43:25 +0530

    commit

---
+++ mobile-os/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import style from './App.css';
+
+function App(){
+    return(
+        <div className='center'>
+            <h1>Mobile Operating System</h1>
+            <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iphone</li>
+            <li>Windows Phone</li></ul>
+
+            <h1>Mobile Manufactures</h1>
+            <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li></ul>
+        </div>
+        
+    )
+}
+
+export  default App;
