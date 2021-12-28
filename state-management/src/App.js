f7a85bf13f859e750473b1e6c57bfccb3273e36b
Swathi <swathi709357@gmail.com>
2021-12-28 22:44:19 +0530

    commit

---
+++ state-management/src/App.js
@@ -0,0 +1,13 @@
+import { Grocery} from "./Grocery/Grocery"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Grocery</h1>
+     <  Grocery/>
+    </div>
+  );
+}
+
+export default App;
